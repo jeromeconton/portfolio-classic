56d2f50f8c6d32e5aef541d35ae1a3b562df0576
unknown <conton.jerome@yahoo.fr>
2024-01-25 14:52:27 +0100

    g

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,10 +10,5 @@ export default defineConfig({
   server: {
     host: true
   },
-  includes: ['src/**/*.astro', 'src/**/*.md'],
-  routes: [
-    { src: '/index', dest: '/src/pages/index.astro' },
-    { src: '/about', dest: '/src/pages/about.astro' },
-    { src: '/work', dest: '/src/pages/work.astro' },
-  ],
+
 });
