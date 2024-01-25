9506df22792d52f3bd61a44daf451d2afdd8cf59
unknown <conton.jerome@yahoo.fr>
2024-01-25 13:56:39 +0100

    Maj astro.config.msj pour le déploiement sur Kinsta

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@ import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 
 export default defineConfig({
-  output: 'server', // ou 'hybrid'
+  output: 'hybrid', // ou 'hybrid'
   adapter: node({
     mode: "standalone"
   }),
   server: {
     host: true
-  }
+  },
+  includes: ['src/**/*.astro', 'src/**/*.md'],
+  routes: [
+    { src: '/index', dest: '/src/pages/index.astro' },
+    { src: '/about', dest: '/src/pages/about.astro' },
+    { src: '/work', dest: '/src/pages/work.astro' },
+  ],
 });
