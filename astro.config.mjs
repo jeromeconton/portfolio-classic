3093b4883a8276dcb142584be2f698956e4b4bea
unknown <conton.jerome@yahoo.fr>
2024-01-25 19:13:56 +0100

    essaie deployement

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,22 +10,5 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
   },
-  routes: [
-    {
-      src: '/',
-      dest: '/src/pages/index.astro',
-    },
-    {
-      src: '/about',
-      dest: '/src/pages/about.astro',
-    },
-    {
-      src: '/work',
-      dest: '/src/pages/work.astro',
-    },
-    {
-      src: '/404',
-      dest: '/src/pages/404.astro',
-    },
-  ],
+  
 });
