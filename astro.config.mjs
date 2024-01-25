33bb3c533abe976722c4d5aabca8683953249369
unknown <conton.jerome@yahoo.fr>
2024-01-25 21:34:08 +0100

    essaie deployement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+  import node from "@astrojs/node";
 
-export default defineConfig({
-  output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
-  server: {
-    host: '0.0.0.0',
-  },
-  
-});
+  export default defineConfig({
+    output: 'server',
+    adapter: node({
+      mode: "standalone"
+    }),
+    server: {
+      host: '0.0.0.0'
+    }
+  });
